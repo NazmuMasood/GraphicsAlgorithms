8e250f5c58f69adc67d49811468f39d0933c13d7
Nazmuddin Al Masood <nazmumasood96@gmail.com>
2019-11-10 04:21:44 +0600

    interesting spurious images

--- src/Main.java
+++ src/Main.java
@@ -30,11 +30,137 @@ class ThirdGLEventListener implements GLEventListener {
 
         gl.glEnd();*/
 
+        //Drawing the axes
+        BruteForceDrawLine(gl, -100, 0, 100, 0);
+        DDA_Algo(gl, 0, -100, 0, 100);
+
         //BruteForceDrawLine(gl, -100, -50, -50, 180);
         //DDA_Algo(gl, -100, -50, -50, 180);
         //MidpointLineForZone0(gl, 100, 50, 200, 80);
         //MidpointLineForAnyZone(50, -100, 100, -190);
+        //MidpointCircleForZone1Arc(50,-50, 100);
+        MidpointCircleForAllZone(100, 50, 100); //change x, y here to get interesting images
+    }
 
+    //MidpointCircleDrawing_Algo irrespective of zones
+    private void MidpointCircleForAllZone(double x1, double y1, double r){
+        int zone = 0;
+        while (zone<8) {
+            switch (zone) {
+                //encoding the zones to zone 0
+                case 0:
+                    MidpointCircleForZone1ArcButModified(0, x1, y1, r);
+                    break;
+                case 1:
+                    MidpointCircleForZone1ArcButModified(1, x1, y1, r);
+                    break;
+                case 2:
+                    MidpointCircleForZone1ArcButModified(2, x1, y1, r);
+                    break;
+                case 3:
+                    MidpointCircleForZone1ArcButModified(3, x1, y1, r);
+                    break;
+                case 4:
+                    MidpointCircleForZone1ArcButModified(4, x1, y1, r);
+                    break;
+                case 5:
+                    MidpointCircleForZone1ArcButModified(5, x1, y1, r);
+                    break;
+                case 6:
+                    MidpointCircleForZone1ArcButModified(6, x1, y1, r);
+                    break;
+                case 7:
+                    MidpointCircleForZone1ArcButModified(7, x1, y1, r);
+                    break;
+            }//switch
+            zone++;
+        }//while
+    }
+    private void MidpointCircleForZone1ArcButModified(int zone, double x_center, double y_center, double r){
+        double d= 1-r;
+        double dE = 3;
+        double dSE = 5 - 2*r;
+        double x=0, y=r;
+        gl.glBegin(GL2.GL_POINTS);
+
+        while (Math.abs(x)<(r/1.4142)){
+
+            //x+=x_center; y+=y_center;
+            System.out.println(x+", "+y);
+            switch (zone){
+                //decoding the zones from zone 1
+                case 0:
+                    gl.glVertex2d(y+y_center, x+x_center);
+                    break;
+                case 1:
+                    gl.glVertex2d(x+x_center, y+y_center);
+                    break;
+                case 2:
+                    gl.glVertex2d(-(x+x_center), y+y_center);
+                    break;
+                case 3:
+                    gl.glVertex2d(-(y+y_center), x+x_center);
+                    break;
+                case 4:
+                    gl.glVertex2d(-(y+y_center), -(x+x_center));
+                    break;
+                case 5:
+                    gl.glVertex2d(-(x+x_center), -(y+y_center));
+                    break;
+                case 6:
+                    gl.glVertex2d(x+x_center, -(y+y_center));
+                    break;
+                case 7:
+                    gl.glVertex2d(y+y_center, -(x+x_center));
+                    break;
+            }
+
+            if (d<0){ //dE
+                d = d + dE;
+                dE += 2;
+                dSE +=2;
+            }
+
+            else { //dSE
+                d = d + dSE;
+                dE += 2;
+                dSE +=4;
+                y--;
+            }
+
+            x++;
+        }// while
+        gl.glEnd();
+    }
+
+    //MidpointCircleDrawing_Algo  [ pixel draw starts clock-wise from (0,r) ]
+    private void MidpointCircleForZone1Arc(double x_center, double y_center, double r){
+        double d= 1-r;
+        double dE = 3;
+        double dSE = 5 - 2*r;
+        double x=0, y=r;
+        gl.glBegin(GL2.GL_POINTS);
+
+        while (Math.abs(x)<(r/1.4142)){
+            System.out.println(x+x_center+", "+y+y_center);
+            gl.glVertex2d(x+x_center, y+y_center);
+
+            if (d<0){ //dE
+                d = d + dE;
+                dE += 2;
+                dSE +=2;
+            }
+
+            else { //dSE
+                d = d + dSE;
+                dE += 2;
+                dSE +=4;
+                y--;
+            }
+
+            x++;
+        }// while
+        gl.glEnd();
 
     }
 
@@ -230,7 +356,7 @@ class ThirdGLEventListener implements GLEventListener {
         gl.glViewport(-250, -150, 250, 150);
         gl.glMatrixMode(GL2.GL_PROJECTION);
         gl.glLoadIdentity();
-        glu.gluOrtho2D(-250.0, 250.0, -150.0, 150.0);
+        glu.gluOrtho2D(-350.0, 350.0, -250.0, 250.0);
     }
 
     /**
@@ -271,7 +397,7 @@ public class Main
         glcanvas.setSize(900, 600);
         //creating frame
         final JFrame frame=new MyJFrame();
-        frame.setTitle("My frame");
+        frame.setTitle("LifeView frame");
         //adding canvas to frame
         frame.add(glcanvas);
         frame.setSize(640,480);
